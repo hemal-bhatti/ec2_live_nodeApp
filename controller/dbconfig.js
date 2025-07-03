934f64925c37a69ce00d2d096504278ee0863e93
hemal bhatti <122991904+hemal-bhatti@users.noreply.github.com>
2025-07-03 15:35:28 +0530

    Update dbconfig.js

--- controller/dbconfig.js
+++ controller/dbconfig.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: "localhost",
-    user: "root",
-    password: "#Hemal",
-    database: "app_db"
+  host: "hemal-db.cbwaeykesql2.ap-south-1.rds.amazonaws.com",
+    user: "admin",
+    password: "#Hemal2004",
+    database: "app_db",
+  port: 3306
 });
 
 module.exports = pool;
